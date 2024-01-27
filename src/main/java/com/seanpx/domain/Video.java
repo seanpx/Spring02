7597f53214e69a6afd0cc804b27687071fc445f6
seanpx <37602629+seanpx@users.noreply.github.com>
2024-01-27 20:34:08 +0800

    2024-01-27 R01 - Chapter4-Ep3 ending - first http Get to return the video list Json. (reviewed again as it's been a while since 16th Jan first try out).

--- src/main/java/com/seanpx/domain/Video.java
+++ src/main/java/com/seanpx/domain/Video.java
@@ -79,7 +79,7 @@ public class Video {
                 ", price=" + price +
                 ", coverImg='" + coverImg + '\'' +
                 ", createTime=" + createTime +
-                '}';
+                "}";
     }
 
 
