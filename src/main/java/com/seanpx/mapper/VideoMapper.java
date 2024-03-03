61787912e94dccfc0b028084d66ad39cab59f2fe
seanpx <37602629+seanpx@users.noreply.github.com>
2024-03-03 13:14:37 +0800

    2024-02-13 R01 - Chapter4-Ep6 stopped at 7mins. Successfully followed the lecture, to create a new API: api/v1/pub/video/save_video_chapter to save video chapters from Request body of the Post Method. 2024-02-25 R01 - Added more displays and study the logic flows based on the console outputs. (very useful) 2024-02-25 R02 - added more displays and understood that all classes (if wired / annotated in spring) will be initialized altogether upon start of the service. (such as Video Class, which is invoked by VideoService interface, is initialized under VideoController) 2024-03-03 R01 - added more displays at VideoMapper, learnt from GPT: as a result of @Repository annotation, upon the start of Spring Service, the VideoMapper is then added into Spring Beans (initialized) with static statement executed. Then we can add @PostConstruct to execute the ending statement upon the construct is completed.

--- src/main/java/com/seanpx/mapper/VideoMapper.java
+++ src/main/java/com/seanpx/mapper/VideoMapper.java
@@ -1,6 +1,7 @@
 package com.seanpx.mapper;
 
 import com.seanpx.domain.Video;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -10,11 +11,23 @@ public class VideoMapper {
     private static Map<Integer, Video> videoMap = new LinkedHashMap<>();
 
     static {
+        System.out.println("##VideoMapper.Static: " + "initializer of VideoMapper");
         videoMap.put(1, new Video(1,"Video#01"));
+        System.out.println("##VideoMapper.Static: " + "executed 1");
         videoMap.put(2, new Video(2,"Video#02"));
+        System.out.println("##VideoMapper.Static: " + "executed 2");
         videoMap.put(3, new Video(3,"Video#03"));
+        System.out.println("##VideoMapper.Static: " + "executed 3");
         videoMap.put(4, new Video(4,"Video#04"));
+        System.out.println("##VideoMapper.Static: " + "executed 4");
         videoMap.put(5, new Video(5,"Video#05"));
+        System.out.println("##VideoMapper.Static: " + "executed 5");
+    }
+
+    @PostConstruct
+    public void init() {
+        // This will be executed after the bean is fully initialized
+        System.out.println("VideoMapper bean has been initialized");
     }
 
     public List<Video> listVideo(){
