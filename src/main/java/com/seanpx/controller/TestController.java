4bbd35651bdf9caec9f52bd59dbdced7b9048872
seanpx <37602629+seanpx@users.noreply.github.com>
2024-10-13 13:17:03 +0800

    2024-02-13 R01 - Chapter4-Ep6 stopped at 7mins. Successfully followed the lecture, to create a new API: api/v1/pub/video/save_video_chapter to save video chapters from Request body of the Post Method. 2024-02-25 R01 - Added more displays and study the logic flows based on the console outputs. (very useful) 2024-02-25 R02 - added more displays and understood that all classes (if wired / annotated in spring) will be initialized altogether upon start of the service. (such as Video Class, which is invoked by VideoService interface, is initialized under VideoController) 2024-03-03 R01 - added more displays at VideoMapper, learnt from GPT: as a result of @Repository annotation, upon the start of Spring Service, the VideoMapper is then added into Spring Beans (initialized) with static statement executed. Then we can add @PostConstruct to execute the ending statement upon the construct is completed.
    2024-07-14 R01 - studied "Map" class which is used to store and fetch key/value pairs and support nesting concept as well. main changes is in VideoMapper.java with some more display statements as well.
    
    2024-08-03 R01 - Chapter4-Ep6 fully completed. in VideoController - save_video_chapter (http Post) is now able to return the video Json object that was sent via Post body (via PostMan)
    
    2024-09-28 R01 - Chapter4-Ep7 7:19. added list_user GetMapping inside UserController.java. I'm thinking whether this is Jackson method or not. and can I use Jackson method to return the Json Data. (To be Continued)
    
    2024-09-28 R02 - Chapter4-Ep7 10:38. Learnt through GPT and applied the same course content XiaoDi wants to teach. (understood some of the concept & flows and also how @JsonInclude works)
    
    2024-09-28 R03 - Chapter4-Ep7 14:18. Learnt middle way and trying to understand further on how Interface is working when /video/list is called. Now I basically know the purpose of having interface is for decoupled design and more maintenabilities. Also, for future creation of new Interface and to invoke Postgres DB tables, I included Postgres dependency in pom.xml, and configurations in application.properties
    
    2024-09-28 R04 - Chapter4-Ep7 17:01.  Learnt from ChatGPT (compared with XiaoDi's code) for the proper Deserialization of Json Data.
    
    2024-10-12 R01 - Chapter6-Ep1 8:14. Learnt hot deployment (although mine not working) and other configurations
    
    2024-10-13 R01 - Chapter6-Ep2 1:14. Learnt adding properties
    
    2024-10-13 R01 - Chapter6-Ep2 7:57. Followed XiaoDi half-way and assisted by GPT, I'm able to return pay.properties back in Json format (postman - get_wxpay_config; TestController)

---
+++ src/main/java/com/seanpx/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.seanpx.controller;
+
+import com.seanpx.utils.JsonData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/test")
+@PropertySource({"classpath:pay.properties"})
+public class TestController {
+
+    @Value("${wxpay.appid}")
+    private String payAppid;
+    @Value("${wxpay.secret}")
+    private String paySecret;
+    @Value("${wxpay.merchantid}")
+    private String merchantID;
+
+    @GetMapping("get_wxpay_config")
+    public JsonData testConfig(){
+
+        Map<String,String> map = new LinkedHashMap<>();
+        map.put("appid",payAppid);
+        map.put("secret",paySecret);
+        map.put("merchant_id",merchantID);
+
+        return JsonData.buildSuccess(map);
+
+    }
+
+}
